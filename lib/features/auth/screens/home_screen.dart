ae724bed5f4df7a108ff39febb7b95977f868b1e
premcode99 <37778735+premcode99@users.noreply.github.com>
2024-01-09 01:55:26 +0530

    home page UI added and carousel scroller lib
    Signed-off-by: premcode99 <37778735+premcode99@users.noreply.github.com>

--- lib/features/auth/screens/home_screen.dart
+++ lib/features/auth/screens/home_screen.dart
@@ -1,6 +1,12 @@
+import 'package:amazon_clone/features/auth/widgets/address_box.dart';
+import 'package:amazon_clone/features/auth/widgets/carousel_image.dart';
+import 'package:amazon_clone/features/auth/widgets/deal_of_the_day.dart';
+import 'package:amazon_clone/features/auth/widgets/top_categories.dart';
 import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../../../constants/global_variables.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = '/home';
@@ -14,10 +20,90 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
-    return  Scaffold(
-      body: Center(
-        child: Text(user.toJson()),
-      ),
-    );
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: AppBar(
+            elevation: 0,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                gradient: GlobalVariables.appBarGradient,
+              ),
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 42,
+                    margin: const EdgeInsets.only(left: 15),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(7),
+                      elevation: 1,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: InkWell(
+                              onTap: () {},
+                              child: const Padding(
+                                padding: EdgeInsets.only(left: 6),
+                                child: Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                  size: 23,
+                                ),
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: const EdgeInsets.only(
+                              top: 10,
+                            ),
+                            border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(7),
+                              ),
+                              borderSide: BorderSide.none,
+                            ),
+                            enabledBorder: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(7),
+                              ),
+                              borderSide:
+                                  BorderSide(color: Colors.black38, width: 1),
+                            ),
+                            hintText: "Search Amazon.in",
+                            hintStyle: const TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 17)),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.transparent,
+                  height: 42,
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Icon(
+                    Icons.mic,
+                    color: Colors.black,
+                    size: 25,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              AddressBox(),
+              SizedBox(
+                height: 10,
+              ),
+              TopCategories(),
+              CarouselImage(),
+              DealOfTheDay()
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/features/auth/widgets/deal_of_the_day.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DealOfTheDay extends StatefulWidget {
+  const DealOfTheDay({super.key});
+
+  @override
+  State<DealOfTheDay> createState() => _DealOfTheDayState();
+}
+
+class _DealOfTheDayState extends State<DealOfTheDay> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.only(left: 10, top: 15),
+          child: Text(
+            "Deal of the day",
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        Image.network('https://images.unsplash.com/photo-1682687982502-1529b3b33f85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D',
+        height: 235,fit: BoxFit.fitHeight,
+        )
+      ],
+    );
+  }
+}
